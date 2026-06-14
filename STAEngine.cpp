79faee9f108c5266e8c8374c3a4f44ea98e3934f
Sanjay Rajnarayanan <sanjayrajrao@gmail.com>
2026-06-14 09:24:33 +0530

    Topological Sort done

--- STAEngine.cpp
+++ STAEngine.cpp
@@ -1,16 +1,44 @@
 #include "STAEngine.hpp"
-#include "Node.hpp"
+#include <queue>
+#include <stdexcept>
 
-STAEngine::STAEngine(TimingGraph &g) : graph(g) {};
+STAEngine::STAEngine(TimingGraph &g) : graph(g) {}
 
 std::vector<NodeID> STAEngine::topologicalSort() {
-  // Constructing the Adjacency list
 
-  const size_t numberOfNodes = graph.size();
+  const NodeID numberOfNodes = static_cast<NodeID>(graph.size());
 
-  std::vector<std::vector<NodeID>> Adjacency(
-      numberOfNodes, std::vector<NodeID>(numberOfNodes, 0));
+  std::vector<int> inDegree(numberOfNodes, 0);
 
-  for (NodeID nodeid = 0; nodeid < numberOfNodes; nodeid++) {
+  std::queue<NodeID> nodeQueue;
+
+  std::vector<NodeID> result;
+
+  for (NodeID i = 0; i < numberOfNodes; i++) {
+    for (const auto &neighbour : graph.getNode(i).fanout) {
+      inDegree[neighbour]++;
+    }
   }
+
+  for (NodeID i = 0; i < numberOfNodes; i++) {
+    if (inDegree[i] == 0) {
+      nodeQueue.push(i);
+    }
+  }
+
+  while (nodeQueue.empty() == false) {
+    NodeID currentNode = nodeQueue.front();
+    nodeQueue.pop();
+    result.push_back(currentNode);
+    for (const auto &neighbour : graph.getNode(currentNode).fanout) {
+      inDegree[neighbour]--;
+      if (inDegree[neighbour] == 0) {
+        nodeQueue.push(neighbour);
+      }
+    }
+  }
+  if (result.size() != graph.size()) {
+    throw std::runtime_error("Combinational loop detected");
+  }
+  return result;
 }
