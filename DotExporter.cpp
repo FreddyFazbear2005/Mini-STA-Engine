433372a3b266048c34149fea2d3e42e8d3853416
Sanjay Rajnarayanan <sanjayrajrao@gmail.com>
2026-07-29 18:29:28 +0530

    Completed for Node, now for edge

--- DotExporter.cpp
+++ DotExporter.cpp
@@ -1,4 +1,5 @@
 #include "DotExporter.hpp"
+#include "Node.hpp"
 #include <fstream>
 #include <stdexcept>
 
@@ -8,20 +9,54 @@ void DotExporter::exportGraph(const TimingGraph &graph,
   if (!file) {
     throw std::runtime_error("Cannot open " + filename);
   }
-  file << "digraph TimingGraph {\n";
-  file << "    rankdir=LR;\n\n";
 
+  DotExporter::writeHeader(file);
+  DotExporter::writeNodes(file, graph);
+  DotExporter::writeEdges(file, graph);
+  DotExporter::writeFooter(file);
+}
+
+void DotExporter::writeHeader(std::ostream &out) {
+  out << "digraph TimingGraph {\n";
+  out << "    rankdir=LR;\n\n";
+
+  out << "    node [\n";
+  out << "         shape=box,\n";
+  out << "         style=filled,\n";
+  out << "         fillcolor=lightgray\n";
+  out << "    ];\n\n";
+
+  out << "    edge [\n";
+  out << "         arrowsize=0.8\n";
+  out << "    ];\n\n";
+}
+
+void DotExporter::writeNodes(std::ostream &out, const TimingGraph &graph) {
   for (const auto &node : graph.getNodes()) {
-    file << "    " << node.name << ";\n";
+    out << "    " << node.name << " [\n";
+    if (node.type == NodeType::primaryInput ||
+        node.type == NodeType::primaryOutput) {
+      out << "        shape=ellipse,\n";
+    } else if (node.type == NodeType::flipFlopD ||
+               node.type == NodeType::flipFlopQ) {
+      out << "        shape=doublecircle,\n";
+    }
+    out << "        label=\"" << node.name;
+    if (!node.cellType.empty()) {
+      out << "\\n" << node.cellType;
+    }
+    out << "\"\n";
+    out << "    ];\n";
   }
-  file << "\n";
+  out << "\n";
+}
 
+void DotExporter::writeEdges(std::ostream &out, const TimingGraph &graph) {
   for (const auto &edge : graph.getEdges()) {
     const Node &src = graph.getNode(edge.source);
     const Node &dst = graph.getNode(edge.destination);
-
-    file << "    " << src.name << " -> " << dst.name << ";\n";
+    out << "    " << src.name << " -> " << dst.name << ";\n";
   }
-  file << "}\n";
-  file.close();
 }
+
+void DotExporter::writeFooter(std::ostream &out) { out << "}\n"; }
--- DotExporter.hpp
+++ DotExporter.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include "TimingGraph.hpp"
+#include <iosfwd>
 
 namespace DotExporter {
 void exportGraph(const TimingGraph &graph, const std::string &filename);
-}
+void writeHeader(std::ostream &out);
+void writeNodes(std::ostream &out, const TimingGraph &graph);
+void writeEdges(std::ostream &out, const TimingGraph &graph);
+void writeFooter(std::ostream &out);
+} // namespace DotExporter
